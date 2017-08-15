0d218c95321e51bc01038bc6d6d33a3c80787bd1
Ubuntu <ubuntu@ip-172-31-63-195.ec2.internal>
2017-08-15 07:25:45 +0000

    Modified with myinfor

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
-S3_LOCATION = 'http://your-amazon-site.amazonaws.com/'
-S3_KEY = 'YOURAMAZONKEY'
-S3_SECRET = 'YOURAMAZONSECRET'
-S3_UPLOAD_DIRECTORY = 'what_directory_on_s3'
-S3_BUCKET = 's3_bucket_name'
+S3_LOCATION =  os.environ.get('S3_LOCATION')
+S3_KEY =  os.environ.get('AWS_ACCESS_KEY_ID')
+S3_SECRET =os.environ.get('AWS_SECRET_ACCESS_KEY') 
+S3_UPLOAD_DIRECTORY = ''
+S3_BUCKET =  os.environ.get('S3_BKT')
 
 SECRET_KEY = "FLASK_SECRET_KEY"
 DEBUG = True
-PORT = 5000
+PORT = 5002
